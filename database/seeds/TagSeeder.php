fc0f8767d8f5407c0c70ccaa4235e1e0b6a6382d
emascapa <emascapa@gmail.com>
2022-06-24 15:35:13 +0200

    Add migration and seed for tag

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Str;
+
+use App\Models\Tag;
+
 class TagSeeder extends Seeder
 {
     /**
@@ -12,5 +16,18 @@ class TagSeeder extends Seeder
     public function run()
     {
         //
+        $tags = ['news', 'funny', 'science', 'meme', 'focus', 'progress'];
+
+        foreach ($tags as $tag) {
+
+            $new_tag = new Tag();
+
+            $new_tag->name = $tag;
+
+            $new_tag->slug = Str::slug($new_tag->name, '-');
+
+            $new_tag->save();
+
+        }
     }
 }
